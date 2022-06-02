12d72697142fa8b5d8c541d85f49b36495f8ae00
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-06-02 15:16:03 -0400

    attempt

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,31 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import AppClass from "./AppClass";
+
+beforeEach(() => {
+  render(<AppClass />);
+});
+
+afterEach(() => {
+  window.localStorage.clear();
+});
+
+describe("AppClass component", () => {
+  test("sanity", () => {
+    expect(true).toBe(true);
+  });
+
+  test("Renders the heading", () => {
+    const heading = screen.queryByText("Coordinates", { exact: false });
+    expect(heading).toBeVisible();
+    expect(heading).toBeInTheDocument();
+  })
+
+  test("Renders the counter", () => {
+    const counter = screen.queryByText("You moved 0 times", { exact: false });
+    expect(counter).toBeVisible();
+    expect(counter).toBeInTheDocument();
+  })
+});
